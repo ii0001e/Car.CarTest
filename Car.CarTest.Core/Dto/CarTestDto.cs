af342c651f4fb81af0854475176844f2e7220858
Ivan Ivanenko <ivan.ivanenko1996@gmail.com>
2024-01-23 09:03:08 +0200

    Add project files.

---
+++ Car.CarTest.Core/Dto/CarTestDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car.CarTest.Core.Dto
+{
+    public class CarTestDto
+    {
+        public string Id { get; set; }
+        public string Brand { get; set; }
+        public string Mark { get; set; }
+        public int Motor { get; set; }
+        public int MaxSpeed { get; set; }
+        public string FuelType { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime Modifieted { get; set; }
+
+    }
+}
